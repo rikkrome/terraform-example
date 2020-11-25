58690b76780b4d48d5d06f4dea2fb3c50edaec8f
ricky romero <contact@romeroricky.com>
2020-11-25 14:28:40 -0800

    example files

---
+++ example.tf
@@ -0,0 +1,38 @@
+terraform {
+  backend "remote" {
+    organization = "romerorickyio"
+
+    workspaces {
+      name = "terraform-example"
+    }
+  }
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 2.70"
+    }
+  }
+}
+
+provider "aws" {
+  profile = "default"
+  region  = var.region
+}
+
+resource "aws_instance" "example" {
+  ami           = "ami-08d70e59c07c61a3a"
+  instance_type = "t2.micro"
+}
+
+resource "aws_eip" "ip" {
+  vpc      = true
+  instance = aws_instance.example.id
+}
+
+output "ip" {
+  value = aws_eip.ip.public_ip
+}
+
+output "id" {
+  value = aws_instance.example.id
+}
